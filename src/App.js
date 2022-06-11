ee9b8d3d276bf94d0bdca4356e2c011183b2335e
Michael Malamud <michael.malamud@outlook.com>
2022-06-11 18:05:41 -0500

    finished buttons and added some styling

--- src/App.js
+++ src/App.js
@@ -2,7 +2,13 @@
 function App() {
 
   const createDigits = () => {
-    const
+    const digits = [];
+    for (let i = 1; i < 10; i++) {
+      digits.push(
+      <button key={i}>{i}</button>
+      )
+    }
+    return digits;
   }
   return (
     <div className="App">
@@ -19,6 +25,7 @@ function App() {
            <button>DEL</button>
          </div>
          <div className="digits">
+           {createDigits()}
            <button>0</button>
            <button>.</button>
            <button>=</button>
